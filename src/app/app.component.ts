49f7b8ac19f60c875fe4a105e76c6e687d2df770
Nicolo <nicolo@icloud.com>
2022-06-07 22:34:02 +0200

    primo commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component, Input } from '@angular/core';
+import { Employee } from './Employee';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  data = [
+    {
+      "id": 10001,
+      "birthDate": "1953-09-01",
+      "firstName": "Georgi",
+      "lastName": "Facello",
+      "gender": "M",
+      "hireDate": "1986-06-25",
+    },
+    {
+      "id": 10002,
+      "birthDate": "1964-06-01",
+      "firstName": "Bezalel",
+      "lastName": "Simmel",
+      "gender": "F",
+      "hireDate": "1985-11-20",
+    },
+    {
+      "id": 10003,
+      "birthDate": "1959-12-02",
+      "firstName": "Parto",
+      "lastName": "Bamford",
+      "gender": "M",
+      "hireDate": "1986-08-27",
+    },
+    {
+      "id": 10004,
+      "birthDate": "1954-04-30",
+      "firstName": "Chirstian",
+      "lastName": "Koblick",
+      "gender": "M",
+      "hireDate": "1986-11-30",
+  
+    },
+    {
+      "id": 10005,
+      "birthDate": "1955-01-20",
+      "firstName": "Kyoichi",
+      "lastName": "Maliniak",
+      "gender": "M",
+      "hireDate": "1989-09-11",
+  
+    }
+  ];
+
+  dipendente = false;
+  datiDipendente: Employee | null = null;
+
+
+  modifica(el : Employee){    
+    this.datiDipendente = el;
+  }
+
+  elimina(id:number){
+    for(let i = 0; i < this.data.length; i++){
+      if(this.data[i].id == id){
+        this.data.splice(i, 1);
+      }
+    }
+  }
+  
+}
+  
